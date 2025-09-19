cd0fa802775df8f3c30c0ea1bd40f5fdf6792926
eehatch <hat19006@byui.edu>
2025-09-18 19:18:50 -0600

    fixed the light/dark toggle and logo change

--- mission/mission.js
+++ mission/mission.js
@@ -1,17 +1,18 @@
-const themeSelector = document.querySelector("dark");
+const themeSelector = document.querySelector("select");
+const logo = document.querySelector("img.logo");
 
 function changeTheme() {
     if (themeSelector.value === "dark") {
         document.body.classList.add("dark");
-        document.logo.src = "byui-logo_white.png";
+        document.body.classList.remove("light");
+        logo.src = "byui-logo_white.png";
     } else {
         document.body.classList.remove("dark");
         document.body.classList.add("light");
-        document.logo.src = "byui-logo_blue.png";
+        logo.src = "byui-logo_blue.webp";
     }
     console.log(themeSelector.value);
 }
-changeTheme();
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
 themeSelector.addEventListener('change', changeTheme);
+changeTheme();
+
