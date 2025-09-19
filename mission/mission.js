b055c9e2243518dcde9d34ae7f3bd2b8d7f41eff
eehatch <hat19006@byui.edu>
2025-09-18 19:09:22 -0600

    finished week 1 assignment, BYUI mission statement page with light/dark mode toggle

--- mission/mission.js
+++ mission/mission.js
@@ -1,4 +1,17 @@
-const apples = 10;
-const oranges = 5;
+const themeSelector = document.querySelector("dark");
 
-console.log(apples + oranges);
+function changeTheme() {
+    if (themeSelector.value === "dark") {
+        document.body.classList.add("dark");
+        document.logo.src = "byui-logo_white.png";
+    } else {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+        document.logo.src = "byui-logo_blue.png";
+    }
+    console.log(themeSelector.value);
+}
+changeTheme();
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
