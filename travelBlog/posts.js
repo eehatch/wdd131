eda298ca517f7d9c1d80ac5eadbc58e2a5473da6
eehatch <hat19006@byui.edu>
2025-10-11 20:40:23 -0600

    Worked on my final site plan

---
+++ travelBlog/posts.js
@@ -0,0 +1,38 @@
+const posts = [
+	{
+		id: 1,
+		Location: "UK",
+		year: '2024',
+		description:
+			"I lived in London the summer of 2024, where I worked at DNEG and spent all my free time wondering the city and going to shows",
+		imgSrc: 'images/london.jpg',
+		imgAlt: 'A picture in the UK'
+	}, 
+	{
+		id: 2,
+		Location: "Grand Canyon",
+		year: '2025',
+		description:
+			"I lived near the North Rim of the Grand Canyon where I worked in a bakery. I was also evacuated for 2 weeks due to the Dragon Bravo and White Sage fires in the area. I spent my free time in national parks!",
+		imgSrc: 'images/gc.jpeg',
+		imgAlt: 'A picture in AZ'
+	}, 
+	{
+		id: 3,
+		Location: "D.C & Florida",
+		year: '2021',
+		description:
+			"My family spent 2 weeks on the East Coast in August of 2021. We spent a week in Washington D.C where we stayed with my uncle, then we flew down to Orlando FL for a week which we spend at DisneyWorld",
+		imgSrc: 'images/disney.jpeg',
+		imgAlt: 'A picture with my Family'
+	} 
+    // 	{
+	// 	id: 4,
+	// 	Location: "UK",
+	// 	year: '2022',
+	// 	description:
+	// 		"I lived in London the summer of 2024, where I worked at DNEG and spent all my free time wondering the city and going to shows",
+	// 	imgSrc: '#',
+	// 	imgAlt: 'A picture in the UK'
+	// }
+]
