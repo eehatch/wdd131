f8fb3e6384792c812b7896a127a590b3e670fc64
eehatch <hat19006@byui.edu>
2025-10-23 09:26:33 -0600

    using the JS

--- travelBlog/travel.js
+++ travelBlog/travel.js
@@ -1,3 +1,4 @@
+
 const posts = [
 	{
 		id: 1,
@@ -19,7 +20,7 @@ const posts = [
 	}, 
 	{
 		id: 3,
-		location: "D.C & Florida",
+		location: "D.C. & Florida",
 		year: '2021',
 		description:
 			"My family spent 2 weeks on the East Coast in August of 2021. We spent a week in Washington D.C where we stayed with my uncle, then we flew down to Orlando FL for a week which we spend at DisneyWorld",
@@ -29,7 +30,6 @@ const posts = [
 ]
 
 function reviewTemplate(post) {
-//   const tagsHTML = post.tags.map(tag => `<span class="tag">${tag}</span>`).join(' ');
   return `
     <div class="post-card">
       <img src="${post.imgSrc}" alt="${post.imgAlt}">
@@ -45,29 +45,49 @@ function reviewTemplate(post) {
 const container = document.getElementById("posts-container");
 container.innerHTML = posts.map(reviewTemplate).join('');
 
+// filterSelection("all")
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("post-card");
+//   if (c == "all") c = "";
+//   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//   for (i = 0; i < x.length; i++) {
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//   }
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// // Show filtered elements
+// function w3AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
+// // Hide elements that are not selected
+// function w3RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
+
+// // Add active class to the current control button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
